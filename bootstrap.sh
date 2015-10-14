f591b46ad03372029518fd45c090a687fed979a3
Thomas van Dam <t.thomas.van.dam@gmail.com>
2015-10-14 23:18:57 +0200

    Activate node and npm in all sessions.

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,7 +4,7 @@ cd "$(dirname "${BASH_SOURCE}")";
 
 function doIt() {
 	rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" \
-		--exclude "mjolnir/" --exclude "README.md" --exclude "LICENSE-MIT.txt" \
+		--exclude "hammerspoon/" --exclude "README.md" --exclude "LICENSE-MIT.txt" \
 		-avh --no-perms . ~;
 	source ~/.bash_profile;
 }
