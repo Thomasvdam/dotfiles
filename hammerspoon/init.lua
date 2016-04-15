3e43a9053d598d958f94b3477e9c9359cf4ed594
Thomas van Dam <t.vandam@triple-it.nl>
2016-04-15 09:56:17 +0200

    Add moving between monitors for work setup.

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -1,9 +1,23 @@
--- Default modifier keys
-local mash = {"cmd", "alt", "shift"}
+---------------
+-- Constants --
+---------------
+local cmnd_alt_shift = {"cmd", "alt", "shift"}
+local cmnd_shift = {"cmd", "shift"}
+local main_monitor = "Color LCD"
+local second_monitor = "DELL U2415"
+
+-------------------
+-- Configuration --
+-------------------
+hs.window.animationDuration = 0
+
+------------
+-- Reload --
+------------
 
 -- Quick config reload
-hs.hotkey.bind(mash, "R", function ()
-  hs.reload()
+hs.hotkey.bind(cmnd_alt_shift, "R", function ()
+    hs.reload()
 end)
 
 ----------------------
@@ -12,6 +26,16 @@ end)
 -- Wrapper for the native OSX notification
 local function notify(title, text)
   hs.notify.new({title = title, informativeText = text}):send()
+end
+
+-- Get a monitor by name
+function hs.screen.get(screen_name)
+  local allScreens = hs.screen.allScreens()
+  for i, screen in ipairs(allScreens) do
+    if screen:name() == screen_name then
+      return screen
+    end
+  end
 end
 
 ----------------------------
@@ -38,7 +62,7 @@ if caffeine then
     caffeine:setIcon("emblemEmpty.png")
 end
 
-hs.hotkey.bind(mash, "/", function() caffeineClicked() end)
+hs.hotkey.bind(cmnd_alt_shift, "/", function() caffeineClicked() end)
 
 -----------------------
 -- Window Management --
@@ -81,11 +105,23 @@ local function moveWindow(pos)
   win:setFrame(f)
 end
 
-hs.hotkey.bind(mash, "Right", function () moveWindow("Right") end)
-hs.hotkey.bind(mash, "Left", function() moveWindow("Left") end)
-hs.hotkey.bind(mash, "Up", function() moveWindow("Up") end)
-hs.hotkey.bind(mash, "Down", function() moveWindow("Down") end)
-hs.hotkey.bind(mash, "F", function() moveWindow("Full") end)
+hs.hotkey.bind(cmnd_alt_shift, "Right", function () moveWindow("Right") end)
+hs.hotkey.bind(cmnd_alt_shift, "Left", function() moveWindow("Left") end)
+hs.hotkey.bind(cmnd_alt_shift, "Up", function() moveWindow("Up") end)
+hs.hotkey.bind(cmnd_alt_shift, "Down", function() moveWindow("Down") end)
+hs.hotkey.bind(cmnd_alt_shift, "F", function() moveWindow("Full") end)
+
+-- Move atom to the left monitor
+hs.hotkey.bind(cmnd_shift, "A", function ()
+  local win = hs.window.focusedWindow()
+  win:moveToScreen(hs.screen.get(second_monitor));
+end)
+
+-- Move atom to the right monitor
+hs.hotkey.bind(cmnd_shift, "D", function ()
+  local win = hs.window.focusedWindow()
+  win:moveToScreen(hs.screen.get(main_monitor));
+end)
 
 -- Indicate config is loaded
 hs.alert("Config loaded")
