763038fed29925708d34cb4d2c8ca0766f3df3a3
Thomas van Dam <t.thomas.van.dam@gmail.com>
2016-05-14 17:56:19 +0200

    Changed window positioning logic to one I found online. Also move windows to monitors based on position, not name.

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -2,9 +2,8 @@
 -- Constants --
 ---------------
 local cmnd_alt_shift = {"cmd", "alt", "shift"}
+local cmnd_alt = {"cmd", "alt"}
 local cmnd_shift = {"cmd", "shift"}
-local main_monitor = "Color LCD"
-local second_monitor = "DELL U2415"
 
 -------------------
 -- Configuration --
@@ -26,16 +25,6 @@ end)
 -- Wrapper for the native OSX notification
 local function notify(title, text)
   hs.notify.new({title = title, informativeText = text}):send()
-end
-
--- Get a monitor by name
-function hs.screen.get(screen_name)
-  local allScreens = hs.screen.allScreens()
-  for i, screen in ipairs(allScreens) do
-    if screen:name() == screen_name then
-      return screen
-    end
-  end
 end
 
 ----------------------------
@@ -64,63 +53,192 @@ end
 
 hs.hotkey.bind(cmnd_alt_shift, "/", function() caffeineClicked() end)
 
------------------------
--- Window Management --
------------------------
--- Move and resize window "Right", "Left", "Up", "Down", or "Full"
-local function moveWindow(pos)
-  local win = hs.window.focusedWindow()
-  local f = win:frame()
-  local screen = win:screen()
-  local max = screen:frame()
-
-  -- Set x position
-  if pos == "Right" then
-    f.x = max.x + (max.w / 2)
-  else
-    f.x = max.x
-  end
-
-  -- Set y position
-  if pos == "Down" then
-    f.y = max.y + (max.h / 2)
-  else
-    f.y = max.y
-  end
-
-  -- Set height
-  if pos == "Up" or pos == "Down" then
-    f.h = max.h / 2
-  else
-    f.h = max.h
-  end
-
-  -- Set width
-  if pos == "Left" or pos == "Right" then
-    f.w = max.w / 2
-  else
-    f.w = max.w
-  end
-
-  win:setFrame(f)
+---------------------------------
+-- Window Management Functions --
+---------------------------------
+-- Adapted from https://github.com/rtoshiro/hammerspoon-init by rtoshiro
+function hs.screen.copyFrame(refScreen)
+    local frame = refScreen:frame()
+    return {
+        x = frame.x,
+        y = frame.y,
+        w = frame.w,
+        h = frame.h
+    }
 end
 
-hs.hotkey.bind(cmnd_alt_shift, "Right", function () moveWindow("Right") end)
-hs.hotkey.bind(cmnd_alt_shift, "Left", function() moveWindow("Left") end)
-hs.hotkey.bind(cmnd_alt_shift, "Up", function() moveWindow("Up") end)
-hs.hotkey.bind(cmnd_alt_shift, "Down", function() moveWindow("Down") end)
-hs.hotkey.bind(cmnd_alt_shift, "F", function() moveWindow("Full") end)
+-- +-----------------+
+-- |        |        |
+-- |        |  HERE  |
+-- |        |        |
+-- +-----------------+
+function hs.window.right(win)
+  local targetFrame = hs.screen.copyFrame(win:screen())
+  targetFrame.x = targetFrame.x + (targetFrame.w/2)
+  targetFrame.w = targetFrame.w/2
+  win:setFrame(targetFrame)
+end
 
--- Move atom to the left monitor
-hs.hotkey.bind(cmnd_shift, "A", function ()
-  local win = hs.window.focusedWindow()
-  win:moveToScreen(hs.screen.get(second_monitor));
+-- +-----------------+
+-- |        |        |
+-- |  HERE  |        |
+-- |        |        |
+-- +-----------------+
+function hs.window.left(win)
+  local targetFrame = hs.screen.copyFrame(win:screen(), false)
+  targetFrame.w = targetFrame.w/2
+  win:setFrame(targetFrame)
+end
+
+-- +-----------------+
+-- |      HERE       |
+-- +-----------------+
+-- |                 |
+-- +-----------------+
+function hs.window.up(win)
+  local targetFrame = hs.screen.copyFrame(win:screen(), false)
+  targetFrame.h = targetFrame.h/2
+  win:setFrame(targetFrame)
+end
+
+-- +-----------------+
+-- |                 |
+-- +-----------------+
+-- |      HERE       |
+-- +-----------------+
+function hs.window.down(win)
+  local targetFrame = hs.screen.copyFrame(win:screen(), false)
+  targetFrame.y = targetFrame.y + targetFrame.h/2
+  targetFrame.h = targetFrame.h/2
+  win:setFrame(targetFrame)
+end
+
+-- +-----------------+
+-- |  HERE  |        |
+-- +--------+        |
+-- |                 |
+-- +-----------------+
+function hs.window.upLeft(win)
+  local targetFrame = hs.screen.copyFrame(win:screen(), false)
+  targetFrame.w = targetFrame.w/2
+  targetFrame.h = targetFrame.h/2
+  win:setFrame(targetFrame)
+end
+
+-- +-----------------+
+-- |                 |
+-- +--------+        |
+-- |  HERE  |        |
+-- +-----------------+
+function hs.window.downLeft(win)
+  local targetFrame = hs.screen.copyFrame(win:screen(), false)
+  win:setFrame({
+    x = targetFrame.x,
+    y = targetFrame.y + targetFrame.h/2,
+    w = targetFrame.w/2,
+    h = targetFrame.h/2
+  })
+end
+
+-- +-----------------+
+-- |                 |
+-- |        +--------|
+-- |        |  HERE  |
+-- +-----------------+
+function hs.window.downRight(win)
+  local targetFrame = hs.screen.copyFrame(win:screen(), false)
+  win:setFrame({
+    x = targetFrame.x + targetFrame.w/2,
+    y = targetFrame.y + targetFrame.h/2,
+    w = targetFrame.w/2,
+    h = targetFrame.h/2
+  })
+end
+
+-- +-----------------+
+-- |        |  HERE  |
+-- |        +--------|
+-- |                 |
+-- +-----------------+
+function hs.window.upRight(win)
+  local targetFrame = hs.screen.copyFrame(win:screen(), false)
+  win:setFrame({
+    x = targetFrame.x + targetFrame.w/2,
+    y = targetFrame.y,
+    w = targetFrame.w/2,
+    h = targetFrame.h/2
+  })
+end
+
+-- +-----------------+
+-- |                 |
+-- |       HERE      |
+-- |                 |
+-- +-----------------+
+function hs.window.fullscreen(win)
+  local targetFrame = hs.screen.copyFrame(win:screen(), false)
+  win:setFrame(targetFrame)
+end
+-- End adapted from
+
+---------------------
+-- Hotkey Bindings --
+---------------------
+hs.hotkey.bind(cmnd_alt_shift, "Right", function ()
+    local win = hs.window.focusedWindow()
+    win:right()
 end)
 
--- Move atom to the right monitor
+hs.hotkey.bind(cmnd_alt_shift, "Left", function ()
+    local win = hs.window.focusedWindow()
+    win:left()
+end)
+
+hs.hotkey.bind(cmnd_alt_shift, "Up", function ()
+    local win = hs.window.focusedWindow()
+    win:up()
+end)
+
+hs.hotkey.bind(cmnd_alt_shift, "Down", function ()
+    local win = hs.window.focusedWindow()
+    win:down()
+end)
+
+hs.hotkey.bind(cmnd_alt, "Right", function ()
+    local win = hs.window.focusedWindow()
+    win:downRight()
+end)
+
+hs.hotkey.bind(cmnd_alt, "Left", function ()
+    local win = hs.window.focusedWindow()
+    win:upLeft()
+end)
+
+hs.hotkey.bind(cmnd_alt, "Up", function ()
+    local win = hs.window.focusedWindow()
+    win:upRight()
+end)
+
+hs.hotkey.bind(cmnd_alt, "Down", function ()
+    local win = hs.window.focusedWindow()
+    win:downLeft()
+end)
+
+hs.hotkey.bind(cmnd_alt_shift, "F", function ()
+    local win = hs.window.focusedWindow()
+    win:fullscreen()
+end)
+
+-- Move focused window a monitor to the left.
+hs.hotkey.bind(cmnd_shift, "A", function ()
+  local win = hs.window.focusedWindow()
+  win:moveToScreen(hs.screen({x=-1,y=0}));
+end)
+
+-- Move focused window a monitor to the right.
 hs.hotkey.bind(cmnd_shift, "D", function ()
   local win = hs.window.focusedWindow()
-  win:moveToScreen(hs.screen.get(main_monitor));
+  win:moveToScreen(hs.screen({x=1,y=0}));
 end)
 
 -- Indicate config is loaded
