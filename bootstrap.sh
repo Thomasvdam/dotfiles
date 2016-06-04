3d664a04c4ca4d318efb54887f3c9c7db89aa6d5
Thomas van Dam <t.thomas.van.dam@gmail.com>
2016-06-04 20:11:29 +0200

    Do not copy colourschemes into home dir.

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,6 +5,7 @@ cd "$(dirname "${BASH_SOURCE}")";
 function doIt() {
 	rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" \
 		--exclude "hammerspoon/" --exclude "README.md" --exclude "LICENSE-MIT.txt" \
+		--exclude "colourschemes/" \
 		-avh --no-perms . ~;
 	source ~/.bash_profile;
 }
